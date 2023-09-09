034b7b00c9692bc1bb31d358e4267e6d6b23b6f2
amir <amirhamiri74@gmail.com>
2023-09-09 17:38:26 +0430

    redirect http://localhost:8000/ to polls page

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/polls/')),
 ]
